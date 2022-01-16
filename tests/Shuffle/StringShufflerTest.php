60da210a7197bb6ea6c068c11722f93c3c1097bb
Jibao Mansaray <jibaomansaray@gmail.com>
2022-01-16 20:28:15 +1000

    Add additional tests for chellenge 3

--- tests/Shuffle/StringShufflerTest.php
+++ tests/Shuffle/StringShufflerTest.php
@@ -22,5 +22,54 @@ class StringShufflerTest extends TestCase
         $this->assertEquals('', $output);
     }
 
+    public function test_examples_in_readmeFile() 
+    {
+        $casesAndExpections = [
+            'abcdefgh' => 'hfdbaceg',
+            'glasier' => 'eslgair',
+            'hotdog stand' => 'dasgdohto tn'
+        ];
+
+        foreach($casesAndExpections as $case => $expects) {
+            $this->assertEquals($expects, $this->shuffler->shuffle($case));
+        }
+    }
+
+
+    public function test_numbers_only()
+    {
+        $casesAndExpections = [
+            12345 => 42135,
+            11111 => 11111,
+            246 => 426
+        ];
+
+        foreach($casesAndExpections as $case => $expects) {
+            $this->assertEquals($expects, $this->shuffler->shuffle($case));
+        }
+    }
+
+    public function test_alphanumeric_strings()
+    {
+
+        $casesAndExpections = [
+            '1a2b3c' => 'cba123',
+            'UID8945' => '48IUD95'
+        ];
+
+        foreach($casesAndExpections as $case => $expects) {
+            $this->assertEquals($expects, $this->shuffler->shuffle($case));
+        }
+    }
+
+
+    public function test_none_alphanumberic_string()
+    {
+
+        $case = '!@#$%^';
+        $expects = '^$@!#%';
+
+        $this->assertEquals($expects, $this->shuffler->shuffle($case));
+    }
 
 }
