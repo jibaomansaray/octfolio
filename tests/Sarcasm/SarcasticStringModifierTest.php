2e8a6501a9a46b5ebe43b46042def380f46b5528
Jibao Mansaray <jibaomansaray@gmail.com>
2022-01-16 18:08:09 +1000

    Add additional tests

--- tests/Sarcasm/SarcasticStringModifierTest.php
+++ tests/Sarcasm/SarcasticStringModifierTest.php
@@ -22,5 +22,33 @@ class SarcasticStringModifierTest extends TestCase
         $this->assertEquals('', $result);
     }
 
+    public function test_examples_in_the_readmeFile()
+    {
+        $example1 = 'Oh ReAlLy';
+        $example2 = 'WeLl ThAnK yOu';
+
+        $this->assertEquals($this->modifier->convert($example1), $example1);
+        $this->assertEquals($this->modifier->convert($example2), $example2);
+
+    }
+
+    public function test_convertion_is_correct()
+    {
+        $casesAndExpectations = [
+            'oh really' =>  'Oh ReAlLy',
+            'well thank you' => 'WeLl ThAnK yOu',
+            'a' => 'A',
+            'A' => 'A',
+            'The quick brown fox jumps...' => 'ThE qUiCk BrOwN fOx JuMpS...',
+            '(*&^%$%$lovely' => '(*&^%$%$lOvElY',
+            '' => '',
+            '    ' => '    ',
+            '   Happy DaYs' => '   HaPpY dAyS'
+        ];
+ 
+        foreach($casesAndExpectations as $case => $expectation) {
+            $this->assertEquals($this->modifier->convert($case), $expectation);
+        }
+    }
 
 }
